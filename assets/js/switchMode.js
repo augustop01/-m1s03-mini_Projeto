3e7c83de21ba936ca85d3cf52b678f9120637922
Augusto Pires <mpires.augusto@gmail.com>
2023-08-14 02:42:57 -0300

    fix: add darkMode ao sessionStorage

--- assets/js/switchMode.js
+++ assets/js/switchMode.js
@@ -7,16 +7,26 @@ const nav = document.querySelector(".menu");
 swtButton.addEventListener("click", SwitchMode);
 swtButton.classList.add("btn", "btn-switch")
 
+const darkMode = sessionStorage.getItem("darkMode");
+
+if(darkMode){
+  main.classList.add("nm")
+  footer.classList.add("nm")
+  swtButton.classList.add("b-nm")
+}
+
 function SwitchMode() {
-  if(main.classList.contains("nm")){
+  if(darkMode){
     main.classList.remove("nm");
     footer.classList.remove("nm");
     swtButton.classList.remove("b-nm")
+    sessionStorage.removeItem("darkMode")
     return
   }
   main.classList.add("nm")
   footer.classList.add("nm")
   swtButton.classList.add("b-nm")
+  sessionStorage.setItem("darkMode", "true")
 }
 
 nav.appendChild(swtButton);
